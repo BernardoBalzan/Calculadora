3bdca650506b938e47283a19bf28e2cf7604edad
Bernardo Balzan <b.balzan@edu.pucrs.br>
2024-04-09 12:35:14 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,38 @@
+function inserir(num) 
+{
+   var numero = document.getElementById("resultado").innerHTML;
+   document.getElementById("resultado").innerHTML = numero + num;  
+}
+
+function apagarResultado()
+{
+    document.getElementById("resultado").innerHTML = "";
+}
+
+function apagarUm()
+{
+    var resultado = document.getElementById("resultado").innerHTML;
+    document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function apagarTudo()
+{
+    document.getElementById("resultado").innerHTML = "";
+    document.getElementById("expressao-anterior").innerHTML = "";
+}
+
+function calcular()
+{
+    var resultado = document.getElementById("resultado").innerHTML;
+    if(resultado)
+    {
+        document.getElementById("expressao-anterior").innerHTML = resultado;
+        document.getElementById("resultado").innerHTML = eval(resultado);
+    }
+    else
+    {
+        document.getElementById("resultado").innerHTML = "";
+    } 
+}
+
+
