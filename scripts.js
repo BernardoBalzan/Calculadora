6cb8e72a863d69f6e1f781e9a57acc4dc7139b86
Bernardo Balzan <b.balzan@edu.pucrs.br>
2024-10-15 20:23:06 -0300

    Calculadora em funcionamento

--- scripts.js
+++ scripts.js
@@ -1,8 +1,27 @@
-function inserir(num) 
-{
-   var numero = document.getElementById("resultado").innerHTML;
-   document.getElementById("resultado").innerHTML = numero + num;  
+function inserir(num) {
+    var resultado = document.getElementById("resultado").innerHTML;
+    if (num === "." || num === "+" || num === "-" || num === "*" || num === "/") {
+        if (resultado.slice(-2) === '**' && (num === '*' || num === '.' || num === '+' || num === '-' || num === '/')) {
+            return;
+        }
+        if (ultimoCaractereEspecial(resultado)) {
+            return; 
+        }
+    }
+    document.getElementById("resultado").innerHTML = resultado + num;
 }
+
+function ultimoCaractereEspecial(string) {
+    const ultimoCaractere = string.charAt(string.length - 1);
+    const penultimoCaractere = string.charAt(string.length - 2);
+    
+    if (penultimoCaractere === '*' && (ultimoCaractere === '.' || ultimoCaractere === '+' || ultimoCaractere === '-' || ultimoCaractere === '/')) {
+        return true;
+    }
+    
+    return ultimoCaractere === '.' || ultimoCaractere === '+' || ultimoCaractere === '-' || ultimoCaractere === '/';
+}
+
 
 function apagarResultado()
 {
@@ -26,13 +45,39 @@ function calcular()
     var resultado = document.getElementById("resultado").innerHTML;
     if(resultado)
     {
-        document.getElementById("expressao-anterior").innerHTML = resultado;
-        document.getElementById("resultado").innerHTML = eval(resultado);
+        if (eval(resultado) === Infinity) {
+            alert("Não é possível fazer essa operação")
+            document.getElementById("resultado").innerHTML = "";
+        }
+        else {
+            if (eval(resultado) % 1 == 0) {
+                document.getElementById("resultado").innerHTML = eval(resultado)
+            }
+            else {
+                document.getElementById("resultado").innerHTML = eval(resultado).toFixed(3);
+            }
+
+            document.getElementById("expressao-anterior").innerHTML = resultado;
+        }
+
     }
     else
     {
         document.getElementById("resultado").innerHTML = "";
     } 
 }
+
+
+
+ function mostrarTooltip(id) {
+     var tooltip = document.getElementById(id);
+     tooltip.style.display = "block";
+}
+
+function tirarTooltip(id) {
+    var tooltip = document.getElementById(id);
+    tooltip.style.display = "none";
+}
+
 
 
